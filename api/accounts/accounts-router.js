9197155d1163ba4ed0469c209fb5a1d914a69645
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-16 15:43:21 -0400

    started modal and router

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,45 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const md = require("./accounts-middleware");
+const ACCOUNT = require("./accounts-model");
 
-router.get('/', (req, res, next) => {
+router.get("/", async (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    const accounts = await ACCOUNT.getAll();
+    res.json(accounts);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get('/:id', (req, res, next) => {
+router.get("/:id", md.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    const account = await ACCOUNT.getById(req.params.id);
+    res.json(account);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
+router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
 });
 
-router.delete('/:id', (req, res, next) => {
+router.put("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.delete("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
 
 module.exports = router;
